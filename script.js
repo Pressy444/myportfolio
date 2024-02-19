67649b2f505408e7890ce8e0930584b1050e8a60
Pressy444 <145189170+Pressy444@users.noreply.github.com>
2024-02-19 18:08:51 +0100

    nqkknsq

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend <br>Developer'],
+    strings: ['Frontend <br>Developer .'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
